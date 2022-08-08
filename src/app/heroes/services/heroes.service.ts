7294d8e812b276398899de0bdb69cc938918af7e
Daniel Interiano <danielstanly@yahoo.es>
2022-08-07 21:26:37 -0600

    path url enviroment

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,21 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Heroe } from '../interfaces/heroe.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
-  path: string = 'http://localhost:3000/heroes';
+  baseUrl: string = environment.baseUrl;
   constructor(private http: HttpClient) {}
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>(this.path);
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
   }
 
   //método del servicio para obtener solo un héroe por id
   getHeroePorId(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
 }
