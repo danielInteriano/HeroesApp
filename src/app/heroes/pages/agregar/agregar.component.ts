44fd6e4387a74348daea4aa2cf0e7dcdf9723c1b
Daniel Interiano <danielstanly@yahoo.es>
2022-08-10 23:02:21 -0600

    Ventana agregar un heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroe.interface';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AgregarComponent implements OnInit {
+  publishers = [
+    {
+      id: 'DC Comics',
+      descripcion: 'DC - Comics',
+    },
+    {
+      id: 'Marvel Comics',
+      descripcion: 'Marvel - Comics',
+    },
+  ];
 
-  constructor() { }
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    publisher: Publisher.DCComics,
+    first_appearance: '',
+    characters: '',
+  };
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
